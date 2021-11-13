d945fddff724620111b609d413f8bbdf2fdfdf6e
AlexKharenko <alexey16052002@gmail.com>
2021-11-13 23:16:17 +0200

    Added message route and limitation for calls

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-// const DB = require('../db/index');
+const limiter = require('../middleware/limiter');
+const transporter = require('../config/mailer');
 
 const router = express.Router();
 
 router.get('/', auth, async (req, res) => {
   const newUser = {
-    login: req.body.login,
+    login: req.body.user.login,
   };
   res.status(200).json({ success: true, user: newUser });
 });
-
+router.post('/message', limiter, async (req, res) => {
+  const mailOption = {
+    to: process.env.EMAIL,
+    subject: req.body.subject,
+    text: `${req.body.text} \nFrom: ${req.body.from}`,
+  };
+  try {
+    await transporter.sendMail(mailOption);
+    res
+      .status(200)
+      .json({ success: true, message: 'The mail was successfully sent!' });
+  } catch (err) {
+    res.status(505).json({ success: false, err: 'Server error' });
+  }
+});
 module.exports = router;
